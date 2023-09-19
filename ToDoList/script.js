7c1816603e6dc0691cad2e6044c897bca8c1664a
Sharmaanshi702 <141817935+Sharmaanshi702@users.noreply.github.com>
2023-09-19 11:04:36 +0000

    adding the js functionalities for selecting the task and removing it

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -15,3 +15,12 @@ else{
 }
 input_Field.value="";
 }
+
+list_Container.addEventListener("click", function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false) 
